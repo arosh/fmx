123b62af2452b87e5d6d1fed79a686bfcf9a1b3c
Sho Iizuka <sho.i518@gmail.com>
2014-12-12 10:52:25 +0900

    select0の実装

--- bitvector.cpp
+++ bitvector.cpp
@@ -32,42 +32,68 @@ TEST(BitVectorBacketTest, RankTest) {
 
   {
     BitVectorBacket bv;
-    for(int i = 0; i < kSizeS*kSizeL; ++i) {
+    for (int i = 0; i < kSizeS * kSizeL; ++i) {
       bv.set(i);
     }
-    EXPECT_EQ(static_cast<uint64_t>(kSizeS*kSizeL), bv.build());
-    for(int i = 0; i < kSizeS*kSizeL; ++i) {
+    EXPECT_EQ(static_cast<uint64_t>(kSizeS * kSizeL), bv.build());
+    for (int i = 0; i < kSizeS * kSizeL; ++i) {
       EXPECT_EQ(i, bv.rank(i));
     }
   }
 }
 
-TEST(BitVectorBacketTest, SelectTest) {
+TEST(BitVectorBacketTest, Select1Test) {
   using namespace bitvector_private;
   {
     BitVectorBacket bv;
     bv.set(0);
     bv.build();
-    EXPECT_EQ(0ULL, bv.select(0));
+    EXPECT_EQ(0ULL, bv.select1(0));
   }
 
   {
     BitVectorBacket bv;
     bv.set(1);
     bv.build();
-    EXPECT_EQ(1ULL, bv.select(0));
+    EXPECT_EQ(1ULL, bv.select1(0));
   }
 
   {
     BitVectorBacket bv;
-    bv.set(kSizeS*kSizeL - 1);
+    bv.set(kSizeS * kSizeL - 1);
     EXPECT_EQ(1ULL, bv.build());
-    EXPECT_TRUE(bv.get(kSizeS*kSizeL - 1));
-    EXPECT_EQ(static_cast<uint64_t>(kSizeS*kSizeL - 1), bv.select(0));
+    EXPECT_TRUE(bv.get(kSizeS * kSizeL - 1));
+    EXPECT_EQ(static_cast<uint64_t>(kSizeS * kSizeL - 1), bv.select1(0));
   }
 }
 
-TEST(BitVectorTest, RankTest) {
+TEST(BitVectorBacketTest, Select0Test) {
+  using namespace bitvector_private;
+  {
+    BitVectorBacket bv;
+    bv.build();
+    EXPECT_EQ(0ULL, bv.select0(0));
+  }
+
+  {
+    BitVectorBacket bv;
+    bv.set(1);
+    bv.build();
+    EXPECT_EQ(2ULL, bv.select0(1));
+  }
+
+  {
+    BitVectorBacket bv;
+    for (int i = 0; i < kSizeS * kSizeL - 1; ++i) {
+      bv.set(i);
+    }
+    EXPECT_EQ(255ULL, bv.build());
+    EXPECT_FALSE(bv.get(kSizeS * kSizeL - 1));
+    EXPECT_EQ(static_cast<uint64_t>(kSizeS * kSizeL - 1), bv.select0(0));
+  }
+}
+
+TEST(BitVectorTest, Rank1Test) {
   {
     BitVector bv(0);
     EXPECT_EQ(0ULL, bv.build());
@@ -77,44 +103,62 @@ TEST(BitVectorTest, RankTest) {
     BitVector bv(2);
     bv.set(0);
     EXPECT_EQ(1ULL, bv.build());
-    EXPECT_EQ(0ULL, bv.rank(0));
-    EXPECT_EQ(1ULL, bv.rank(1));
+    EXPECT_EQ(0ULL, bv.rank1(0));
+    EXPECT_EQ(1ULL, bv.rank1(1));
   }
 
   {
     BitVector bv(3);
     bv.set(1);
     EXPECT_EQ(1ULL, bv.build());
-    EXPECT_EQ(0ULL, bv.rank(0));
-    EXPECT_EQ(0ULL, bv.rank(1));
-    EXPECT_EQ(1ULL, bv.rank(2));
+    EXPECT_EQ(0ULL, bv.rank1(0));
+    EXPECT_EQ(0ULL, bv.rank1(1));
+    EXPECT_EQ(1ULL, bv.rank1(2));
   }
 
   {
     BitVector bv(12345);
-    for(int i = 0; i < 12345; ++i) {
+    for (int i = 0; i < 12345; ++i) {
       bv.set(i);
     }
     EXPECT_EQ(12345ULL, bv.build());
-    for(int i = 0; i < 12345; ++i) {
-      EXPECT_EQ(static_cast<uint64_t>(i), bv.rank(i));
+    for (int i = 0; i < 12345; ++i) {
+      EXPECT_EQ(static_cast<uint64_t>(i), bv.rank1(i));
     }
   }
 }
 
-TEST(BitVector, SelectTest) {
+TEST(BitVectorTest, Rank0Test) {
+  {
+    BitVector bv(2);
+    EXPECT_EQ(0ULL, bv.build());
+    EXPECT_EQ(0ULL, bv.rank0(0));
+    EXPECT_EQ(1ULL, bv.rank0(1));
+  }
+
+  {
+    BitVector bv(3);
+    bv.set(1);
+    EXPECT_EQ(1ULL, bv.build());
+    EXPECT_EQ(0ULL, bv.rank0(0));
+    EXPECT_EQ(1ULL, bv.rank0(1));
+    EXPECT_EQ(1ULL, bv.rank0(2));
+  }
+}
+
+TEST(BitVector, Select1Test) {
   {
     BitVector bv(1);
     bv.set(0);
     bv.build();
-    EXPECT_EQ(0ULL, bv.select(0));
+    EXPECT_EQ(0ULL, bv.select1(0));
   }
 
   {
     BitVector bv(2);
     bv.set(1);
     bv.build();
-    EXPECT_EQ(1ULL, bv.select(0));
+    EXPECT_EQ(1ULL, bv.select1(0));
   }
 
   {
@@ -122,17 +166,32 @@ TEST(BitVector, SelectTest) {
     bv.set(12345 - 1);
     EXPECT_EQ(1ULL, bv.build());
     EXPECT_TRUE(bv.get(12345 - 1));
-    EXPECT_EQ(static_cast<uint64_t>(12345 - 1), bv.select(0));
+    EXPECT_EQ(static_cast<uint64_t>(12345 - 1), bv.select1(0));
   }
 
   {
     BitVector bv(12345);
-    for(int i = 0; i < 12345; ++i) {
+    for (int i = 0; i < 12345; ++i) {
       bv.set(i);
     }
     EXPECT_EQ(12345ULL, bv.build());
-    for(int i = 0; i < 12345; ++i) {
-      EXPECT_EQ(static_cast<uint64_t>(i), bv.select(i));
+    for (int i = 0; i < 12345; ++i) {
+      EXPECT_EQ(static_cast<uint64_t>(i), bv.select1(i));
     }
+  }
+}
+
+TEST(BitVector, Select0Test) {
+  {
+    BitVector bv(1);
+    bv.build();
+    EXPECT_EQ(0ULL, bv.select0(0));
+  }
+
+  {
+    BitVector bv(2);
+    bv.set(0);
+    bv.build();
+    EXPECT_EQ(1ULL, bv.select0(0));
   }
 }
--- bitvector.h
+++ bitvector.h
@@ -68,7 +68,7 @@ public:
     const uint8_t r = S[a] + __builtin_popcountll(B[a] & mask);
     return r;
   }
-  Index select(uint8_t r) const {
+  Index select1(uint8_t r) const {
 #ifndef NDEBUG
     assert(built);
     assert(r < S[kSizeL - 1] + __builtin_popcountll(B[kSizeL - 1]));
@@ -95,6 +95,40 @@ public:
       Index M = (L + R) / 2;
       uint64_t mask = (1ULL << M) - 1;
       if (__builtin_popcountll(BB & mask) <= r) {
+        L = M;
+      } else {
+        R = M;
+      }
+    }
+    return ret + L;
+  }
+  Index select0(uint8_t r) const {
+#ifndef NDEBUG
+    assert(built);
+    assert(r + rank(kSizeS * kSizeL - 1) < kSizeS * kSizeL);
+#endif
+    Index L, R;
+
+    L = 0, R = kSizeL;
+    while (R - L > 1) {
+      Index M = (L + R) / 2;
+      if (kSizeS * M - S[M] <= r) {
+        L = M;
+      } else {
+        R = M;
+      }
+    }
+
+    uint64_t BB = B[L];
+    Index ret = kSizeS * L;
+
+    r -= kSizeS * L - S[L];
+
+    L = 0, R = kSizeS;
+    while (R - L > 1) {
+      Index M = (L + R) / 2;
+      uint64_t mask = (1ULL << M) - 1;
+      if (M - __builtin_popcountll(BB & mask) <= r) {
         L = M;
       } else {
         R = M;
@@ -152,7 +186,7 @@ public:
     Index b = i % kSize;
     return backets[a].get(b);
   }
-  Index rank(Index i) const {
+  Index rank1(Index i) const {
 #ifndef NDEBUG
     assert(built);
     assert(i < n);
@@ -161,12 +195,20 @@ public:
     Index b = i % kSize;
     return S[a] + backets[a].rank(b);
   }
-  Index select(Index r) const {
+  Index rank0(Index i) const {
 #ifndef NDEBUG
     assert(built);
-    assert(r <=
-           S[bitvector_private::ceil(n, kSize) - 1] +
-               backets[bitvector_private::ceil(n, kSize) - 1].rank(kSize - 1));
+    assert(i < n);
+#endif
+    Index a = i / kSize;
+    Index b = i % kSize;
+    return kSize * a - S[a] + b - backets[a].rank(b);
+  }
+  Index rank(bool b, Index i) { return b ? rank1(i) : rank0(i); }
+  Index select1(Index r) const {
+#ifndef NDEBUG
+    assert(built);
+    assert(r <= rank1(n - 1));
 #endif
     Index L, R;
 
@@ -183,7 +225,30 @@ public:
     const auto &backet = backets[L];
     Index ret = kSize * L;
     r -= S[L];
-    return ret + backet.select(r);
+    return ret + backet.select1(r);
   }
+  Index select0(Index r) const {
+#ifndef NDEBUG
+    assert(built);
+    assert(r <= rank0(n - 1));
+#endif
+    Index L, R;
+
+    L = 0, R = bitvector_private::ceil(n, kSize);
+    while (R - L > 1) {
+      Index M = (L + R) / 2;
+      if (kSize * M - S[M] <= r) {
+        L = M;
+      } else {
+        R = M;
+      }
+    }
+
+    const auto &backet = backets[L];
+    Index ret = kSize * L;
+    r -= kSize * L - S[L];
+    return ret + backet.select0(r);
+  }
+  Index select(bool b, Index r) const { return b ? select1(r) : select0(r); }
 }; // class BitVector
 } // namespace
